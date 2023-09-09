ae63d39c5ae20652bd34fe883efcf584d5434c26
Ivan <gureff.al@yandex.ru>
2023-09-09 20:51:20 +0300

    Improve Pool script

--- Assets/Source/Scripts/EnemyScripts/EnemyAI.cs
+++ Assets/Source/Scripts/EnemyScripts/EnemyAI.cs
@@ -1,20 +1,19 @@
+using EvolveGames;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 public class EnemyAI : MonoBehaviour
 {
-    private GameObject _player;
-    private NavMeshAgent _enemy;
+    [SerializeField] private GameObject _player;
+    [SerializeField] private NavMeshAgent _enemy;
     public int HealthEnemy = 100;
-    private void Start()
-    {
-        _enemy = GetComponent<NavMeshAgent>();
-        _player = GameObject.FindGameObjectWithTag("Player");
-    }
+    private void Start() => _player = FindObjectOfType<PlayerController>().gameObject;
     private void Update()
     {
         if (HealthEnemy <= 0)
         {
-            Destroy(gameObject);
+            HealthEnemy = 100;
+            gameObject.SetActive(false);          
             return;
         }
        _enemy.SetDestination(_player.transform.position);
