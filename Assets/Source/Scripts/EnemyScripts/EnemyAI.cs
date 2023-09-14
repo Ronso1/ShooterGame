f5fa5b18fd4d400d3c2a4754eb35aca52907a0b7
Ivan <gureff.al@yandex.ru>
2023-09-14 22:03:59 +0300

    Fix bugs with transform models

--- Assets/Source/Scripts/EnemyScripts/EnemyAI.cs
+++ Assets/Source/Scripts/EnemyScripts/EnemyAI.cs
@@ -6,10 +6,6 @@ public class EnemyAI : MonoBehaviour
     [SerializeField] private NavMeshAgent _enemy;
     private GameObject _player;
     public int HealthEnemy = 100;
-    private void Awake()
-    {
-        _enemy.updateRotation = false;
-    }
     private void Start() => _player = FindObjectOfType<PlayerController>().gameObject;
     private void Update()
     {
@@ -19,12 +15,6 @@ public class EnemyAI : MonoBehaviour
             gameObject.SetActive(false);
             return;
         }
-
-        /*var targetPosition = _enemy.pathEndPosition;
-        var targetPoint = new Vector3(targetPosition.x, transform.position.y, targetPosition.z);
-        var _direction = (targetPoint - transform.position).normalized;
-        var _lookRotation = Quaternion.LookRotation(_direction*);
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(Vector3.forward), 180);*/
         _enemy.SetDestination(_player.transform.position);
     }
 }
